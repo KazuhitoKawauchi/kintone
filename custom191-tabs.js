7593419ed15f1c0865f8b6108564ad1f2f52186b
KazuhitoKawauchi <kawauchi@konp.co.jp>
2025-06-06 17:14:50 +0900

    Update custom191-tabs.js

--- custom191-tabs.js
+++ custom191-tabs.js
@@ -1,15 +1,7 @@
-(function() {
+(function () {
   "use strict";
-  kintone.events.on("app.record.index.show", function(event) {
-    console.log("\u2705 Connected to Kintone index view");
-    var messageDiv = document.createElement("div");
-    messageDiv.textContent = "\u2705 Script is working";
-    var listArea = document.querySelector("#records-table, .recordlist-gaia, #record-gaia");
-    if (listArea && listArea.parentNode) {
-      listArea.parentNode.insertBefore(messageDiv, listArea.nextSibling);
-    } else {
-      document.body.appendChild(messageDiv);
-    }
+  kintone.events.on("app.record.index.show", function (event) {
+    alert("✅ custom191-tabs.js is running!");
     return event;
   });
 })();
